fffd79715b949f7d24fd1e090b4281e7c3d5006f
Oliver Liu <liuoliver348@gmail.com>
2020-03-20 15:00:36 -0700

    initial commit

---
+++ Assets/shooting.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    public float damage = 10.0f;
+    public float range = 100.0f;
+    public Transform fireStart;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        LayerMask mask = LayerMask.GetMask("Terrain", "Enemy");
+        RaycastHit2D hit = Physics2D.Raycast(fireStart.position, fireStart.up, range, mask);
+        if (hit.collider != null)
+        {
+            target gametarget = hit.transform.GetComponent<target>();
+            if (gametarget != null)
+            {
+                gametarget.TakeDamage(damage);
+            }
+        }
+        
+    }
+}
---
+++ Assets/target.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class target : MonoBehaviour
+{
+    public float health = 50.0f;
+    public void TakeDamage(float ammount)
+    {
+        health -= ammount;
+        if (health <= 0.0f)
+        {
+            Die();
+        }
+        
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
